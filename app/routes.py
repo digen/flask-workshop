20e3fc80f392fabaa75152dd7dcc4b0f83b92c4b
Swapneel Patnekar <swapneel.patnekar@shreshtait.com>
2017-12-21 15:11:29 +0530

    Added posts in routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'swapneel'}
-    return render_template('index.html', title='Web Page', user=user)
+    posts = [
+        {
+            'author':{'username': 'Swapneel'}
+            'body': 'Hello World'
+        }
+        {
+            'author':{'username': 'Ira'}
+            'body': 'Python is cool'
+        }
+    ]
+        
+    return render_template('index.html', title='Web Page', user=user,posts=posts)
