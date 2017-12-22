0ff1c5fff7e4491e9a99545bc4e0f418e23a70c1
Swapneel Patnekar <swapneel.patnekar@shreshtait.com>
2017-12-22 15:26:55 +0530

    User Authentication - fixed indendation

--- app/routes.py
+++ app/routes.py
@@ -18,10 +18,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-	next_page = request.args.get('next')
+        next_page = request.args.get('next')
         if not next_page or not next_page.startswith('/'):
             next_page = url_for('index')
-        return redirect(next_page)
+            return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/')
